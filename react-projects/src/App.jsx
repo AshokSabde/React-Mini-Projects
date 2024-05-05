437adaae150ee02f3591c9ae8b9bed86ad1861d6
ashoksabde07 <ashoksabde07@gmail.com>
2024-05-05 12:42:16 +0530

    add initial files

---
+++ react-projects/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Accordian from './Components/StarRatingApp'
+
+
+function App() {
+  return(
+    <div>
+      <Accordian></Accordian>
+  </div>
+  ) 
+
+
+}
+
+export default App
---
+++ react-projects/src/Components/StarRatingApp/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+function StarRatingApp() {
+  return (
+    <div className='star-rating'>StarRatingApp</div>
+  )
+}
+
+export default StarRatingApp;
